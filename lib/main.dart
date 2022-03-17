63475b4c6ee75e802d11368f2b5021f2369f26ec
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-03-17 01:27:25 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:rpg_cast/views/forgotPassword.dart';
 import 'package:rpg_cast/views/login.dart';
 import 'package:rpg_cast/views/register.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
+
+  //* Deixa o app em fullscreen
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 }
 
 class MyApp extends StatelessWidget {
