0506fcbbedaf9c76f21fadb7845a5d8f93b2daa5
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-06-06 22:48:22 -0300

    Update login.dart

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:rpg_cast/views/register.dart';
 
 class Login extends StatefulWidget {
@@ -17,6 +18,7 @@ class _LoginState extends State<Login> {
   //*Controladores do texto
   TextEditingController emailController = TextEditingController();
   TextEditingController passwdController = TextEditingController();
+  bool isLoggedIn = false;
 
   //* cor primaria e secundaria
   Color primaryColor = const Color.fromARGB(255, 255, 111, 0);
@@ -24,6 +26,9 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    final controllerUsername = TextEditingController();
+    final controllerPassword = TextEditingController();
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
@@ -73,7 +78,7 @@ class _LoginState extends State<Login> {
                                 ),
                               ),
 
-                              //* Campo de Usuario
+                              //* Campo de Email
                               Padding(
                                 padding:
                                     const EdgeInsets.symmetric(vertical: 10),
@@ -182,6 +187,7 @@ class _LoginState extends State<Login> {
                                   ),
                                 ),
                               ),
+                              //* Botão login
                               Padding(
                                 padding: const EdgeInsets.symmetric(
                                   vertical: 15,
@@ -191,16 +197,11 @@ class _LoginState extends State<Login> {
                                     primary:
                                         const Color.fromARGB(255, 238, 103, 0),
                                   ),
-                                  onPressed: () {
-                                    setState(
-                                      () {
-                                        emailController.clear();
-                                        passwdController.clear();
-                                      },
-                                    );
-                                  },
-                                  child: const Text(
-                                    'Entrar',
+                                  onPressed: isLoggedIn
+                                      ? () => doUserLogin()
+                                      : () => doUserLogout,
+                                  child: Text(
+                                    isLoggedIn ? "Sair" : "Entrar",
                                     style: TextStyle(
                                       fontFamily: "Enchanted",
                                       fontSize: 35,
@@ -256,5 +257,77 @@ class _LoginState extends State<Login> {
         ),
       ),
     );
+  }
+
+  void showSuccess(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Success!"),
+          content: Text(message),
+          actions: <Widget>[
+            new TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error!"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            new TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void doUserLogin() async {
+    final email = emailController.text.trim();
+    final password = passwdController.text.trim();
+
+    final user = ParseUser(null, password, email);
+
+    var response = await user.login();
+
+    if (response.success) {
+      showSuccess("Usuario logado com sucesso");
+      setState(() {
+        isLoggedIn = true;
+      });
+    } else {
+      showError(response.error!.message);
+    }
+  }
+
+  void doUserLogout() async {
+    final user = await ParseUser.currentUser() as ParseUser;
+    var response = await user.logout();
+
+    if (response.success) {
+      showSuccess("Usuario deslogado com sucesso");
+      setState(() {
+        isLoggedIn = false;
+      });
+    } else {
+      showError(response.error!.message);
+    }
   }
 }
